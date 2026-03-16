b4547329293931672a0230c5728338977d2f8d62
App Meu RPPS <appmeurpps@gmail.com>
2026-03-16 14:40:01 -0400

    Alteração do projeto 1.0

--- Automacao/script.py
+++ Automacao/script.py
@@ -1,8 +1,8 @@
-import time
-
 from selenium import webdriver
 
 browser = webdriver.Chrome()
 
 browser.get("https://proxima2.sisprevweb.com.br/estado_01/pericia/Login/Login.aspx")
-time.sleep(60)
+browser.find_element(By.ID, "txtLogin").sendKeys("1340")
+browser.find_element(By.ID, "txtSenha").csendKeys("fgh38a")
+browser.find_element(By.ID, "btnLogin").click()
