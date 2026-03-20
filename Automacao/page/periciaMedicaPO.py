5febd4979fb2bb27209f7d2133ebd458091d344e
App Meu RPPS <appmeurpps@gmail.com>
2026-03-20 15:12:51 -0400

    Alteração do projeto 1.9

--- Automacao/page/periciaMedicaPO.py
+++ Automacao/page/periciaMedicaPO.py
@@ -8,6 +8,8 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
         self.wait   = WebDriverWait(driver, 10)
+        self.login = "1340"
+        self.senha = "fgh38a"
 
     # Declaração dos elementos
     def username(self):
@@ -21,12 +23,57 @@ class LoginPage:
 
     def tituloLogin(self):
         return self.driver.find_element(By.LINK_TEXT, "Perícia Médica")
+    
+    def acessarModuloConfiguracoes(self):
+        return self.wait.until(EC.element_to_be_clickable((By.ID, "liConfiguracoes")))
+    
+    def acessarMenuGruposPermissoes(self):
+        return self.wait.until(EC.element_to_be_clickable((By.ID, "liGrupoPermissoes")))
+    
+    def validarTituloGrupoPermissoes(self):
+        return self.wait.until(EC.visibility_of_element_located((By.XPATH, "//b[text()='Grupos - Permissões']")))
 
+    def buttonNovoGrupoPermissoes(self):
+        return self.wait.until(EC.element_to_be_clickable((By.ID, "btnNovo")))
+    
+    def modalCadastroGruposPermissao(self):
+        return self.wait.until(EC.visibility_of_element_located((By.ID, "modalcadastronome1")))
+    
+    def campoNomeGrupo(self):
+        return self.wait.until(EC.element_to_be_clickable((By.ID, "ContentPlaceHolder1_txtNomeGrupo")))
+    
+    def buttonSalvarCadastroGrupo(self):
+        return self.wait.until(EC.element_to_be_clickable((By.ID, "btnSalvarDocumento")))
+    
+    def linhaGrupoPermissao(self):
+        return self.wait.until(EC.visibility_of_element_located((By.XPATH, "//td[contains(text(),'Automação')]")))
+    
+    # Editar Permissão de Grupo Permissões
+    def buttonEditarGrupoPermissao(self):
+        return self.wait.until(EC.visibility_of_element_located((By.XPATH, "//tr[td[contains(.,'Automação')]]//a[contains(@class,'editor_edit')]")))
+    
+    def buttonExcluirGrupoPermissao(self):
+        return self.wait.until(EC.visibility_of_element_located((By.XPATH, "//tr[td[contains(.,'Automação')]]//a[contains(@class,'editor_remove')]")))
+    
+    def buttonPermissoesGrupoPermissao(self):
+        return self.wait.until(EC.visibility_of_element_located((By.XPATH, "//tr[td[contains(.,'Automação')]]//a[contains(@class,'editor_perm')]")))
+
+    #-------------------------------------------------------------------------------------------------#
+    #Validar mensagem de erro ou sucesso
     def toastTitulo(self):
         return self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "toast-title")))
 
     def toastMensagem(self):
         return self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "toast-message")))
 
-    login = "1340"
-    senha = "fgh38a"
+    def toastTituloOculto(self):
+        return self.wait.until(EC.invisibility_of_element_located((By.CLASS_NAME, "toast-title")))
+
+    def toastMensagemOculto(self):
+        return self.wait.until(EC.invisibility_of_element_located((By.CLASS_NAME, "toast-message")))
+
+    #Métodos
+    def efetuarLogin(self):
+        self.username().send_keys(self.login)
+        self.password().send_keys(self.senha)
+        self.buttonLogin().click()
--- Automacao/test/periciaMedicaTest.py
+++ Automacao/test/periciaMedicaTest.py
@@ -1,5 +1,7 @@
 import time
 import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from page.periciaMedicaPO import LoginPage
 from selenium import webdriver
@@ -23,21 +25,45 @@ def setup():
 # ✅ Verifica que está visível
 # assert relembrar.is_displayed() == True, "Elemento não está visível!"
 
-def test_TC001_validarTitulos(setup):
-    pm, page = setup
-    assert pm.title == "Pericia | Login", f"Errado! Encontrado: '{pm.title}'"
-    assert page.tituloLogin().text == "Perícia Médica", f"Errado! Encontrado: '{page.tituloLogin().text}'"
+    def test_TC001_validarTitulos(setup):
+        pm, page = setup
+        assert pm.title == "Pericia | Login", f"Errado! Encontrado: '{pm.title}'"
+        assert page.tituloLogin().text == "Perícia Médica", f"Errado! Encontrado: '{page.tituloLogin().text}'"
 
-def test_TC002_validarCamposLogin(setup):
+    def test_TC002_validarCamposLogin(setup):
+        pm, page = setup
+        page.buttonLogin().is_displayed(), "Não!"
+        page.buttonLogin().is_enabled(), "Não!"
+        page.buttonLogin().click()
+        assert page.toastTitulo().text == "Atenção!", f"Encontrado: '{page.toastTitulo().text}'"
+        assert page.toastMensagem().text == "Esse usuário não pertence ao grupo Perícia Médica!", f"Encontrado: '{page.toastMensagem().text}'"
+        assert page.toastTituloOculto(), "Atenção"
+        assert page.toastMensagemOculto(), "Esse usuário não pertence ao grupo Perícia Médica!"
+
+    def test_TC003_efetuarLogin(setup):
+        pm, page = setup
+        page.username().send_keys(page.login)
+        page.password().send_keys(page.senha)
+        page.buttonLogin().click()
+
+def test_TC004_cadastrarGrupoPermissao(setup):
     pm, page = setup
-    page.buttonLogin().is_displayed(), "Não!"
-    page.buttonLogin().is_enabled(), "Não!"
-    page.buttonLogin().click()
+    page.efetuarLogin()
+    page.acessarModuloConfiguracoes().click()
+    page.acessarMenuGruposPermissoes().click()
+    assert page.validarTituloGrupoPermissoes().text == "Grupos - Permissões", f"Encontrado: '{page.validarTituloGrupoPermissoes().text}'"
+    page.buttonNovoGrupoPermissoes().click()
+    assert page.modalCadastroGruposPermissao().text == "Cadastro de Grupos de Permissão", f"Encontrado: '{page.modalCadastroGruposPermissao().text}'"
+    page.campoNomeGrupo().send_keys("Automação")
+    page.buttonSalvarCadastroGrupo().click()
     assert page.toastTitulo().text == "Atenção!", f"Encontrado: '{page.toastTitulo().text}'"
-    assert page.toastMensagem().text == "Esse usuário não pertence ao grupo Perícia Médica!", f"Encontrado: '{page.toastMensagem().text}'"
+    assert page.toastMensagem().text == "Grupo cadastrado com sucesso!", f"Encontrado: '{page.toastMensagem().text}'"
+    assert page.linhaGrupoPermissao().text == "Automação", f"Encontrado: '{page.linhaGrupoPermissao().text}'"
 
-def test_TC003_efetuarLogin(setup):
+def test_TC005_editarCadastroGrupoPermissao(setup):
     pm, page = setup
-    page.username().send_keys(page.login)
-    page.password().send_keys(page.senha)
-    page.buttonLogin().click()
+    page.efetuarLogin()
+    page.acessarModuloConfiguracoes().click()
+    page.acessarMenuGruposPermissoes().click()
+    assert page.validarTituloGrupoPermissoes().text == "Grupos - Permissões", f"Encontrado: '{page.validarTituloGrupoPermissoes().text}'"
+    page.campoNomeGrupo().send_keys("Automação")
